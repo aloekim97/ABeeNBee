1fac3cec1cc1d2fd3c4fde6e7ac4fb2acd7898ba
Alex Kim <aloekim97@gmail.com>
2022-10-24 17:09:03 -0700

    more edits

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
-const router = express.Router();
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+const router = express.Router();
 
 const validateLogin = [
   check('credential')
--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const router = express.Router();
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
 
 const validateSignup = [
     check('email')
@@ -20,6 +24,12 @@ const validateSignup = [
       .exists({ checkFalsy: true })
       .isLength({ min: 6 })
       .withMessage('Password must be 6 characters or more.'),
+    check('firstName')
+      .exists({ checkFalsy: true })
+      .withMessage('First Name is required'),
+    check('lastName')
+      .exists({ checkFalsy: true })
+      .withMessage('Last Name is required'),
     handleValidationErrors
   ];
 
