fb02a6dc0c50136e6023a5295ddba55c47db69ed
Alex Kim <aloekim97@gmail.com>
2022-11-21 05:13:45 -0800

    del-button

--- frontend/src/components/Reviews/Reviews.js
+++ frontend/src/components/Reviews/Reviews.js
@@ -1,7 +1,7 @@
 import {useDispatch, useSelector} from 'react-redux'
 import { delRevThunk } from '../../store/reviews';
 import { detailThunk } from '../../store/spots';
-
+import './NewReview.css'
 
 
 export default function SpotRevs({review, spot}) {
